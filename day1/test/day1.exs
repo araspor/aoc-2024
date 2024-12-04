1a088a90b93b3e89bea4e9ae2ead2804bece525f
araspor <andraz.raspor@gmail.com>
2024-12-05 00:15:02 +0100

    day1.2 - just needed to add another method to module

--- day1/test/day1.exs
+++ day1/test/day1.exs
@@ -19,11 +19,22 @@ defmodule Day1 do
   end
 
   def sum_distances([head1 | tail1], [head2 | tail2]) do
-    IO.puts("Tail1=#{inspect(tail1)}\nTail2=#{inspect(tail2)}\n")
+    #IO.puts("Tail1=#{inspect(tail1)}\nTail2=#{inspect(tail2)}\n")
     sum_distances(tail1, tail2) + distance(head1, head2)
   end
 
   def sum_distances([], []) do
+    0
+  end
+
+  def sum_occurences([headLeft | tailLeft], listRight) do
+    count = Enum.count(listRight, fn x -> x === headLeft end)
+    #IO.puts("Element #{inspect(headLeft)} occurs in #{inspect(listRight)} #{inspect(count)}")
+
+    count * headLeft + sum_occurences(tailLeft, listRight)
+  end
+
+  def sum_occurences([], _listRight) do
     0
   end
 end
@@ -36,15 +47,14 @@ list2 = Enum.sort(list2)
 
 IO.puts("Sum of distances between list1 and list2 is #{inspect(Day1.sum_distances(list1, list2))}\n")
 
+IO.puts("Similarity score is #{inspect(Day1.sum_occurences(list1, list2))}\n")
+
 {:ok, fileContent} = File.read("input")
 split = String.split(fileContent, "\n", trim: true)
 
 pairList = Enum.map(split, & String.split(&1, " ", trim: true))
 flattenToSingleList = Enum.flat_map(pairList, & List.flatten(&1))
                       |> Enum.map(&String.to_integer/1)
-
-
-IO.puts("wat #{inspect(flattenToSingleList)}\n\n")
 
 listOne = Stream.take_every(flattenToSingleList, 2)
           |> Enum.to_list
@@ -55,11 +65,13 @@ listTwo = flattenToSingleList -- listOne
 |> Enum.sort
 
 
-IO.puts("List one freq: #{inspect(Enum.frequencies(listOne))}\n")
-IO.puts("List two freq: #{inspect(Enum.frequencies(listTwo))}\n")
+#IO.puts("List one freq: #{inspect(Enum.frequencies(listOne))}\n")
+#IO.puts("List two freq: #{inspect(Enum.frequencies(listTwo))}\n")
 
-IO.puts("List one: #{inspect(listOne)}\n Length=#{inspect(length(listOne))}\n")
-IO.puts("List two: #{inspect(listTwo)}\n Length=#{inspect(length(listOne))}\n")
+#IO.puts("List one: #{inspect(listOne)}\n Length=#{inspect(length(listOne))}\n")
+#IO.puts("List two: #{inspect(listTwo)}\n Length=#{inspect(length(listOne))}\n")
 
 
 IO.puts("Sum of distances between listOne and listTwo is #{inspect(Day1.sum_distances(listOne, listTwo))}")
+
+IO.puts("Similarity score is #{inspect(Day1.sum_occurences(listOne, listTwo))}")
